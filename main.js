6508f8e09593aa73a65e852e4e27b3194f77ee3e
DIegoDevcode <jdiegomoreno05@gmail.com>
2023-06-30 15:42:41 -0500

    Generando los ataques aleatorios del jugador.

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
+let ataqueJugador 
+
 function iniciarJuego() {
     let botónMascotaJugador = document.getElementById('boton-mascota')
     botónMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+
+    let botonFuego = document.getElementById('boton-fuego')
+    botonFuego.addEventListener('click', ataqueFuego)
+    let botonAgua = document.getElementById('boton-agua')
+    botonAgua.addEventListener('click', ataqueAgua)
+    let botonTierra = document.getElementById('boton-tierra')
+    botonTierra.addEventListener('click', ataqueTierra)
 }
 
 function seleccionarMascotaJugador(){
@@ -33,6 +42,20 @@ function seleccionarMascotaPc(){
     }
 }
 
+function ataqueFuego(){
+    ataqueJugador = 'Fuego'
+    alert(ataqueJugador)
+}
+
+function ataqueAgua(){
+    ataqueJugador = 'Agua'
+    alert(ataqueJugador)
+}
+
+function ataqueTierra(){
+    ataqueJugador = 'Tierra'
+    alert(ataqueJugador)
+}
 
 function aleatorio(min,max) {
     return Math.floor(Math.random()*(max-min + 1)+ min)
